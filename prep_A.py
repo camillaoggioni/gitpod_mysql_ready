4955b3b40da462e2702071c9b510b9de36676960
camillaoggioni <camilla1.oggioni@iisgalvanimi.edu.it>
2023-03-03 10:24:13 +0000

    prep a

--- prep_A.py
+++ prep_A.py
@@ -3,10 +3,10 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
-  database="animali"
+  password="",
+  database= "Animali"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+mycursor.execute("CREATE TABLE Mammiferi (id INT AUTO_INCREMENT PRIMARY KEY, Nome_proprio VARCHAR(255), Razza VARCHAR(255), Peso INT, Età INT)")
