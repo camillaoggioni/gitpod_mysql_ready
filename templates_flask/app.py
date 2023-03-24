a6c820b172de46d6814b5d3069789e603742f77f
camillaoggioni <camilla1.oggioni@iisgalvanimi.edu.it>
2023-03-24 10:44:14 +0000

    24 marzo

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -4,7 +4,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import mysql.connector
+import pandas as pd
 
-@app.route('/')
-def hello():
-    return render_template('hello.html', name='Camy')
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult= mycursor.fetchall()
+    return render_template(clash_units.html, units=myresult)
